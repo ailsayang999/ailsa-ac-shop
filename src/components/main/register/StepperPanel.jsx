3a33cde1a13b811b6953363d2c5aa81a8922de21
AilsaYang <ailsayang831031@gmail.com>
2023-07-25 22:32:28 +0800

    Chore: Change active circle content in StepperPanel into check mark icon.

--- src/components/main/register/StepperPanel.jsx
+++ src/components/main/register/StepperPanel.jsx
@@ -1,8 +1,6 @@
 import "./stepperPanel.scss";
 import { useEffect, useState } from "react";
 
-
-
 const StepperPanel = ({ active, stepperCircleNum }) => {
   //stepperCircleNum 的初始值是 3
   const labelName = ["寄送地址", "運送方式", "付款資訊"];
@@ -11,23 +9,20 @@ const StepperPanel = ({ active, stepperCircleNum }) => {
     setWidth((100 / (stepperCircleNum - 1)) * active);
   }, [stepperCircleNum, active]);
 
- 
-
   return (
     <div id="step-control" className="stepper-panel">
       <div className="stepper-container container">
         <div className="progress" style={{ width: width + "%" }}></div>
         {labelName.map((name, i) => {
           return (
-            <div className="circle" key={name}>
-              <div
-                className={`circle-number-container ${i <= active && "active"}`}
-              >
-                {i + 1}
+            <div
+              className={i <= active ? "circle active" : "circle"}
+              key={name}
+            >
+              <div className="circle-number-container">
+                {i <= active ? " " : i + 1}
               </div>
-              <span className={`label-content ${i <= active && "active"}`}>
-                {name}
-              </span>
+              <span className="label-content">{name}</span>
             </div>
           );
         })}
